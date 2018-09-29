e03708a2d9cd7b07a3b9c1c30c4623fb203b8a6a
Joel Rivera <joel.rivera7995@gmail.com>
2018-09-29 14:53:39 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,5 +18,10 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-
+  batch_badge_creator(attendees).each do |value|
+    puts value
+  end
+  assign_rooms(attendees).each do |value|
+    puts value
+  end
 end
