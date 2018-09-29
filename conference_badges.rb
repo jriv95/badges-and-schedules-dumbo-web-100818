a42a89ad23016751b1dd08b1c82bf136442dafe2
Joel Rivera <joel.rivera7995@gmail.com>
2018-09-29 14:28:48 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,10 @@
 def conference_badges
 end
 
-  def badge_maker(name)
+def badge_maker(name)
     return "Hello, my name is #{name}."
-  end
+end
+
+def batch_badge_creator(attendees)
+  return attendees
+end
